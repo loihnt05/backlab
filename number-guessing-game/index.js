331e1984c56bb51f0324a8d0b2724ec29c89739f
loihnt05 <23520869@gm.uit.edu.vn>
2025-07-28 21:36:58 +0700

    game guessing synchronous

--- number-guessing-game/index.js
+++ number-guessing-game/index.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
-import readline from 'node:readline';
-let guessingNumber;
-let choice = 1;
-const random = Math.floor(Math.random() * 100) + 1;
-console.log(random)//
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const rl = readline.createInterface({ input, output });
+const random = Math.floor(Math.random() * 100) + 1;
+
 console.log("Welcome to the Number Guessing Game!");
 console.log("I'm thinking of a number between 1 and 100.");
 console.log("You have the number of chances to guess the correct number.");
@@ -18,24 +14,26 @@ console.log("1. Easy (10 chances) - default");
 console.log("2. Medium (5 chances)");
 console.log("3. Hard (3 chances)");
 
-rl.question("\nEnter your choice: ", (answer) => {
-  if (answer) choice = parseInt(answer);
-  rl.close();
-});
+let choiceStr = await rl.question("\nEnter your choice: ");
+let choice = parseInt(choiceStr);
+if (![1, 2, 3].includes(choice)) choice = 1;
 
-console.log(`Great! You have selected the ${choice} difficulty level.`);
+let attempts = choice === 1 ? 10 : choice === 2 ? 5 : 3;
+console.log(`\nGreat! You have selected level ${choice}. You have ${attempts} chances.`);
 console.log("Let's start the game!");
 
-for (let i = 1; i <= Math.floor(10 / choice); ++i) {
-  rl.question("\nEnter your guess: ", (answer) => {
-    guessingNumber = parseInt(answer);
-    if (guessingNumber === random) {
-        console.log("Congratulations! You guessed the correct number in ${i} attempts.")
-        return;
-    } else {
-        console.log(`Incorrect! The number is ${(guessingNumber < random) ? "less" : "greater"} than ${guessingNumber}.`)
-    }
+for (let i = 1; i <= attempts; ++i) {
+  let guessStr = await rl.question(`\nAttempt ${i}: Enter your guess: `);
+  let guess = parseInt(guessStr);
+
+  if (guess === random) {
+    console.log(`Congratulations! You guessed the correct number in ${i} attempts.`);
     rl.close();
-  });
+    process.exit(0); 
+  } else {
+    console.log(`Incorrect! The number is ${(guess < random) ? "greater" : "less"} than ${guess}.`);
+  }
 }
 
+console.log(`\nGame over! The correct number was ${random}`);
+rl.close();
