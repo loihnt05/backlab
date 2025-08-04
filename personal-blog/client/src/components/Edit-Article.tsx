e4adc921eddee53b92cfe2206dcb5e87c4d24f6e
loihnt05 <23520869@gm.uit.edu.vn>
2025-08-04 12:14:01 +0700

    new and update page

--- personal-blog/client/src/components/Edit-Article.tsx
+++ personal-blog/client/src/components/Edit-Article.tsx
@@ -1,16 +1,36 @@
-
-export default function EditArticle({
-  title,
-}: {
-  title: string;
-}) {
+import { useParams } from "react-router-dom";
+import data from "../data.json"
+export default function EditArticle({ title }: { title: string }) {
+  const { id } = useParams()
+  const article = data.find((a) => a.id === id)
+  const pubDate = article ? new Date(article?.publishDate).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric"
+        }) : ""
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 border-2 border-black border-solid p-10  h-[80%] w-[50%] rounded-xl">
       <h1 className="text-4xl font-bold mb-7">{title} Article</h1>
-      <input className="border-gray-400 border-2 border-solid p-3 rounded-sm" type="text" placeholder="Title Article" />
-      <input className="border-gray-400 border-2 border-solid p-3 rounded-sm " type="text" placeholder="Publishing Date" />
-      <textarea className="border-gray-400 border-2 border-solid p-3 rounded-sm h-100 text-left w-full" placeholder="Content" />
-      <button className="w-[30%] cursor-pointer hover:bg-gray-100 text-black p-3 border-2 border-gray-400 border-solid rounded-md">{title === "Update" ? "Update" : "Publish"}</button>
+      <input
+        className="border-gray-400 border-2 border-solid p-3 rounded-sm"
+        type="text"
+        placeholder="Title Article"
+        defaultValue={article?.title}
+      />
+      <input
+        className="border-gray-400 border-2 border-solid p-3 rounded-sm "
+        type="text"
+        placeholder="Publishing Date"
+        defaultValue={pubDate}
+      />
+      <textarea
+        className="border-gray-400 border-2 border-solid p-3 rounded-sm h-100 text-left w-full"
+        placeholder="Content"
+        defaultValue={article?.content}
+      />
+      <button className="w-[30%] cursor-pointer hover:bg-gray-100 text-black p-3 border-2 border-gray-400 border-solid rounded-md">
+        {title === "Update" ? "Update" : "Publish"}
+      </button>
     </div>
   );
 }
--- personal-blog/client/src/main.tsx
+++ personal-blog/client/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login.tsx";
 import Article from "./components/Article.tsx";
+import EditArticle from "./components/Edit-Article.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,6 +14,8 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<App />} />
         <Route path="/blogs/:id" element={<Article />} />
+        <Route path="/edit/:id" element={<EditArticle  title="Update"/>} />
+        <Route path="/new" element={<EditArticle title="Publish"/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
