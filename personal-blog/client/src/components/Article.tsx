9b18ee58fec6aa888695ef7de95c858ad5f10b84
loihnt05 <23520869@gm.uit.edu.vn>
2025-08-04 12:48:15 +0700

    remove article by useState

--- personal-blog/client/src/components/Article.tsx
+++ personal-blog/client/src/components/Article.tsx
@@ -1,7 +1,8 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import type { Article } from "../App";
 import { Link } from "react-router-dom";
 import data from "../data.json";
+import { useState } from "react";
 
 export default function Article() {
   const { id } = useParams();
@@ -33,18 +34,55 @@ export function ArticleHome({
   admin: boolean;
   listArticle: Article[];
 }) {
+  const [deleteItem, setDeleteItem] = useState<Article>();
+  const [listArticles, setListArticles] = useState<Article[]>(listArticle)
+  const nav = useNavigate();
+  const [showPopup, setShowPopup] = useState(false);
+  const handleDelete = (article: Article) => {
+    setListArticles((prev => prev.filter(item => item.id !== article.id)))
+  };
   return (
     <div className="h-full space-y-10">
+      {showPopup && (
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded shadow-lg max-w-sm w-full">
+            <h2 className="text-xl font-bold mb-4">Notification</h2>
+            <p className="mb-4">Are you sure about deleting blog.</p>
+            <div className="flex gap-2">
+              <button
+                onClick={() => {
+                  setShowPopup(false);
+                }}
+                className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer hover:opacity-60"
+              >
+                No
+              </button>
+              <button
+                onClick={() => {
+                  setShowPopup(false);
+                  handleDelete(deleteItem!);
+                }}
+                className="bg-blue-700 text-white px-4 py-2 rounded cursor-pointer hover:opacity-60"
+              >
+                Yes
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="flex items-stretch h-full gap-4">
         <h1 className="font-bold text-4xl">Personal Blog</h1>
         {admin && (
-          <button className="font-semibold text-lg px-4 ml-auto cursor-pointer hover:opacity-70">
+          <button
+            className="font-semibold text-lg px-4 ml-auto cursor-pointer hover:opacity-70"
+            onClick={() => nav("/new")}
+          >
             + Add
           </button>
         )}
       </div>
       <div className="flex gap-2 flex-col">
-        {listArticle.map((article) => {
+        {listArticles.map((article) => {
           return (
             <div className="flex items-center" key={article.id}>
               <Link
@@ -55,10 +93,19 @@ export function ArticleHome({
               </Link>
               {admin ? (
                 <div className="grid grid-cols-2 grid-rows-1 ml-auto">
-                  <button className="hover:opacity-60 cursor-pointer text-lg font-semibold text-gray-500">
+                  <button
+                    onClick={() => nav(`/edit/${article.id}`)}
+                    className="hover:opacity-60 cursor-pointer text-lg font-semibold text-gray-500"
+                  >
                     Edit
                   </button>
-                  <button className="hover:opacity-60 cursor-pointer text-lg font-semibold text-gray-500">
+                  <button
+                    onClick={() => {
+                      setShowPopup(true)
+                      setDeleteItem(article)
+                    }}
+                    className="hover:opacity-60 cursor-pointer text-lg font-semibold text-gray-500"
+                  >
                     Delete
                   </button>
                 </div>
