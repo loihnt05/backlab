d560d171a4302ee45498e9ff036852085c6f4af4
loihnt05 <23520869@gm.uit.edu.vn>
2025-08-16 20:54:33 +0700

    server with api open weather map

---
+++ weather-api/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import axios from "axios"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+port = 3000
+const app = express()
+
+app.use(cors())
+
+app.get("/weather", async (req, res) => {
+    const city = req.query.city
+    const apiKey = process.env.API_KEY
+
+    try {
+        const response = await axios.get(
+            'https://api.openweathermap.org/data/2.5/weather', 
+            { params: {
+                q: city, appid: apiKey, units: "metric", lang: "vi"
+            }}
+        );
+        res.json(response.data)
+
+    } catch (error) {
+        res.status(500).json({error: "Error with data"})
+
+    }
+})
+
+app.listen(port, () => console.log(`Server run at http://localhost:${port}`))
