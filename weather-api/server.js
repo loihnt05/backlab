ecfaf2893d813618776f1109ca3155b66a11be5e
loihnt05 <23520869@gm.uit.edu.vn>
2025-08-21 15:29:10 +0700

    using open weather api key

--- weather-api/server.js
+++ weather-api/server.js
@@ -1,31 +1,39 @@
-import express from "express"
-import axios from "axios"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
-port = 3000
-const app = express()
+dotenv.config();
 
-app.use(cors())
+const port = 3000;
+const app = express();
 
-app.get("/weather", async (req, res) => {
-    const city = req.query.city
-    const apiKey = process.env.API_KEY
+app.use(cors());
 
-    try {
-        const response = await axios.get(
-            'https://api.openweathermap.org/data/2.5/weather', 
-            { params: {
-                q: city, appid: apiKey, units: "metric", lang: "vi"
-            }}
-        );
-        res.json(response.data)
+app.get("/weather/:city", async (req, res) => {
+  const city = req.params.city;
+  const apiKey = process.env.API_KEY;
 
-    } catch (error) {
-        res.status(500).json({error: "Error with data"})
+  try {
+    const response = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          q: city,
+          appid: apiKey,
+          units: "metric",
+          lang: "vi",
+        },
+      }
+    );
+    res.json({
+        city: response.data.name, 
+        temperature: response.data.main.temp,
+        description: response.data.weather[0].description,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error with data" });
+  }
+});
 
-    }
-})
-
-app.listen(port, () => console.log(`Server run at http://localhost:${port}`))
+app.listen(port, () => console.log(`Server run at http://localhost:${port}`));
