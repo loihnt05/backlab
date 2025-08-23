08206f4cf328b8201b70948b0037cced315f665a
loihnt05 <23520869@gm.uit.edu.vn>
2025-08-23 14:46:16 +0700

    using env api key and switch type

--- tmdb-tool/dist/index.js
+++ tmdb-tool/dist/index.js
@@ -6,12 +6,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const commander_1 = require("commander");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 commander_1.program.name("").description("").version("");
 commander_1.program.option("--type <type>", "get api from type", "default").parse();
 const options = commander_1.program.opts();
 console.log(`Initializing tmdb project with type: ${options.type}`);
 async function main() {
-    const movies = await axios_1.default.get("https://api.themoviedb.org/3/movie/now_playing?api_key=1b9c6e5cd270781d91678374c9dbab7b&language=en-US&page=1");
-    console.log(movies);
+    let endpoint = options.type;
+    if (endpoint === "playing")
+        endpoint = "now_playing";
+    if (endpoint === "top")
+        endpoint = "top_rated";
+    try {
+        const response = await axios_1.default.get(`https://api.themoviedb.org/3/movie/${endpoint}?api_key=${process.env.API_KEY}&language=en-US&page=1`);
+        const movies = response.data.results;
+        if (!movies || movies.length === 0) {
+            console.log("No movies found.");
+            return;
+        }
+        console.log("Showing 10 movies:");
+        movies.slice(0, 10).forEach((movie, idx) => {
+            console.log(`${idx + 1}. ${movie.title}`);
+        });
+    }
+    catch (error) {
+        if (error.response && error.response.data && error.response.data.status_message) {
+            console.error("Error from TMDB API:", error.response.data.status_message);
+        }
+        else {
+            console.error("An error occurred:", error.message);
+        }
+    }
 }
 main();
--- tmdb-tool/src/index.ts
+++ tmdb-tool/src/index.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import axios from "axios";
 import { program } from "commander";
+import dotenv from "dotenv"
 
-program.name("").description("").version("");
+dotenv.config()
+
+program.name("The movie database cli tool").description("using cli to find top 10 movie with types").version("1.0.0");
 
 program.option("--type <type>", "get api from type", "default").parse();
 
@@ -10,10 +13,31 @@ const options = program.opts();
 console.log(`Initializing tmdb project with type: ${options.type}`);
 
 async function main() {
-  const movies = await axios.get(
-    "https://api.themoviedb.org/3/movie/now_playing?api_key=1b9c6e5cd270781d91678374c9dbab7b&language=en-US&page=1"
-  );
-  console.log(movies);
+  let endpoint = options.type;
+  if (endpoint === "playing") 
+    endpoint = "now_playing";
+  if (endpoint === "top")
+    endpoint = "top_rated"
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${endpoint}?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    );
+    const movies = response.data.results;
+    if (!movies || movies.length === 0) {
+      console.log("No movies found.");
+      return;
+    }
+    console.log("Showing 10 movies:");
+    movies.slice(0, 10).forEach((movie: any, idx: number) => {
+      console.log(`${idx + 1}. ${movie.title}`);
+    });
+  } catch (error: any) {
+    if (error.response && error.response.data && error.response.data.status_message) {
+      console.error("Error from TMDB API:", error.response.data.status_message);
+    } else {
+      console.error("An error occurred:", error.message);
+    }
+  }
 }
 
 main();
